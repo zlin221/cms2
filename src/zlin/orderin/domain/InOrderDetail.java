93650bf31f540d50a0e642195c8bcb1ac2c78a6d
huangjinhao <1910787585@qq.com>
2015-05-28 12:42:57 +0800

    完善了InOrderDetail实体类

--- src/zlin/orderin/domain/InOrderDetail.java
+++ src/zlin/orderin/domain/InOrderDetail.java
@@ -1,5 +1,58 @@
 package zlin.orderin.domain;
 
 public class InOrderDetail {
+	private int id;
+	private String articleNumber;
+	private String type;
+	private String color;
+	private String size;
+	private int count;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getArticleNumber() {
+		return articleNumber;
+	}
+
+	public void setArticleNumber(String articleNumber) {
+		this.articleNumber = articleNumber;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
 }
